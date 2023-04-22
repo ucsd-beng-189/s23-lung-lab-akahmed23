a78139fdb6fc5f7a290d090901ea7b398912605e
akahmed23 <akahmed@ucsd.edu>
2023-04-22 16:05:07 -0700

    Task 4

--- lung.m
+++ lung.m
@@ -2,26 +2,19 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-betalist =[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
+betavec =[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
+% 
+% beta=betavec(10)
+% for M=0:0.01:10
+% setup_lung
+% cvsolve
+% outchecklung
+% 
+% end
 
-for i = 1:11
-    beta=betalist(i)
-setup_lung
-cvsolve
-outchecklung
-PIlist(i)=PI;
-PAlist(i)=PAbar;
-Palist(i)=Pabar;
-Pvlist(i)=Pv;
-end
-
-% Plot the results
-figure('NumberTitle','off','Name','Beta variation');
-plot(betalist,PIlist)
-hold on
-plot(betalist,Palist)
-plot(betalist,PAlist)
-plot(betalist,Pvlist)
-xlabel('Beta')
-ylabel('Partial pressure of oxygen (mmHg)')
-legend('Inspired PO2', 'Mean arterial PO2', 'Mean alveolarPO2', 'Venous PO2')
+close all
+Mvec=[0.04,0.04,0.04,0.03,0.03,0.03,0.03,0.03,0.03,0.03,0.02];
+plot(betavec,Mvec,'o-');
+xlabel('beta');
+ylabel('M-max');
+title('beta vs M-max');
