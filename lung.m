86a9b163c3367e4b5244c5f4f0195f94f30be731
akahmed23 <akahmed@ucsd.edu>
2023-04-22 15:37:43 -0700

    Task 3

--- lung.m
+++ lung.m
@@ -2,6 +2,26 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
+betalist =[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
+
+for i = 1:11
+    beta=betalist(i)
 setup_lung
 cvsolve
 outchecklung
+PIlist(i)=PI;
+PAlist(i)=PAbar;
+Palist(i)=Pabar;
+Pvlist(i)=Pv;
+end
+
+% Plot the results
+figure('NumberTitle','off','Name','Beta variation');
+plot(betalist,PIlist)
+hold on
+plot(betalist,Palist)
+plot(betalist,PAlist)
+plot(betalist,Pvlist)
+xlabel('Beta')
+ylabel('Partial pressure of oxygen (mmHg)')
+legend('Inspired PO2', 'Mean arterial PO2', 'Mean alveolarPO2', 'Venous PO2')
