c1fe6141bfed39c82c780f13ee7033814bbadf6e
akahmed23 <akahmed@ucsd.edu>
2023-04-22 16:34:50 -0700

    Task 5

--- lung.m
+++ lung.m
@@ -1,20 +1,34 @@
-%filename: lung.m (main program)
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-betavec =[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
-% 
-% beta=betavec(10)
-% for M=0:0.01:10
-% setup_lung
-% cvsolve
-% outchecklung
-% 
-% end
+CIconstant =[0.209,0.173,0.143,0.118,0.097];
 
-close all
-Mvec=[0.04,0.04,0.04,0.03,0.03,0.03,0.03,0.03,0.03,0.03,0.02];
-plot(betavec,Mvec,'o-');
-xlabel('beta');
-ylabel('M-max');
-title('beta vs M-max');
+for i = 1:5
+    cI=CIconstant(i)/(22.4*(310/273));
+setup_lung
+cvsolve
+outchecklung
+cI(i)=cI
+PIlist(i)=PI;
+PAlist(i)=PAbar;
+Palist(i)=Pabar;
+Pvlist(i)=Pv;
+cAlist(i)=cAbar;
+calist(i)=cabar;
+cvlist(i)=cv;
+end
+
+% Plot the results
+figure('NumberTitle','off','Name','Partial pressure of oxygen vs Concentration of oxygen in air');
+plot(cI,PIlist)
+hold on
+plot(cI,Palist)
+plot(cI,PAlist)
+plot(cI,Pvlist)
+plot(cI,cAlist)
+plot(cI,calist)
+plot(cI,cvlist)
+xlabel('Concentration of oxygen in air (moles/liter)')
+ylabel('Partial pressure of oxygen (mmHg)')
+legend('PI', 'Pabar', 'PAbar', 'Pv','cAbar','cabar','cv')
+title('Partial pressure of oxygen vs Concentration of oxygen in air')
